76d9a6e92e7eaeb45e765977ddda9673674a4291
Eric <127228647+w1a2b3c@users.noreply.github.com>
2024-11-22 21:07:58 +0800

    Delete src directory

--- src/utility/math.ts
+++
@@ -1,8 +0,0 @@
-export function clamp(value: number, min: number, max: number) {
-	min = isNaN(min) ? Number.NEGATIVE_INFINITY : min;
-	max = isNaN(max) ? Number.POSITIVE_INFINITY : max;
-	if (min > max) {
-		[min, max] = [max, min];
-	}
-	return Math.max(Math.min(value, max), min);
-}
